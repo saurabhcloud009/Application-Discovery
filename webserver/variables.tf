cca1d53c4036358d42555bbe38ea53627393d2cb
Pathak, Saurabh (US - Bengaluru) <saupathak@deloitte.com>
2019-06-27 12:53:44 +0530

    update commit

--- webserver/variables.tf
+++ webserver/variables.tf
@@ -1,5 +1,5 @@
-variable "access_key" { default = "AKIAJ7YG6IEUF34X5BDQ" }
-variable "secret_key" { default = "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" }
+variable "access_key" { default = "" }
+variable "secret_key" { default = "" }
 variable "region" {
   default = "us-east-2"
 }
